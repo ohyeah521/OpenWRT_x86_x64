cb2cad83873812be53cb522a73e205b0cc584859
ohyeah521 <ohyeah521@gmail.com>
2024-02-26 08:31:27 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -31,7 +31,6 @@ git clone https://github.com/sbwml/luci-app-daed-next
 
 # Add luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 
 # Add luci-app-unblockneteasemusic
@@ -100,4 +99,4 @@ rm -rf nas-packages-luci/luci/luci-app-istorex
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.23/g' package/base-files/files/bin/config_generate
